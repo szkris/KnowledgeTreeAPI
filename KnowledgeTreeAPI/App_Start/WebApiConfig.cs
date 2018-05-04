00a73c9e4597630958fc4cc86418fb64df893f52
DESKTOP-EECSIA8\szabo <szaboo.kris@gmail.com>
2018-05-04 09:00:04 +0200

    Chrome és minden más böngésző JSON kiíratás megoldása.

--- KnowledgeTreeAPI/App_Start/WebApiConfig.cs
+++ KnowledgeTreeAPI/App_Start/WebApiConfig.cs
@@ -1,15 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace KnowledgeTreeAPI
 {
+    public class JsonContentNegotiator : IContentNegotiator
+    {
+        private readonly JsonMediaTypeFormatter JsonFormatter;
+
+        public JsonContentNegotiator(JsonMediaTypeFormatter jsonFormatter)
+        {
+            JsonFormatter = jsonFormatter;
+        }
+
+        public ContentNegotiationResult Negotiate(Type type, HttpRequestMessage request, IEnumerable<MediaTypeFormatter> formatters)
+        {
+            return new ContentNegotiationResult(JsonFormatter, new MediaTypeHeaderValue("application/json"));
+        }
+    }
+
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+
+            var JsonFormatter = new JsonMediaTypeFormatter();
+            config.Services.Replace(typeof(IContentNegotiator), new JsonContentNegotiator(JsonFormatter));
 
             // Web API routes
             config.MapHttpAttributeRoutes();
